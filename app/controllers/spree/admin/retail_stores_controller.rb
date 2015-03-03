9d1b9b04ac66d5c967a8b5ba3e364fc0af93b43b
Sanad Liaquat <sanadhussain@gmail.com>
2015-03-03 18:03:31 +0500

    Before removing spree address

--- app/controllers/spree/admin/retail_stores_controller.rb
+++ app/controllers/spree/admin/retail_stores_controller.rb
@@ -1,31 +1,56 @@
 module Spree
   module Admin
+    # class RetailStoresController < Spree::Admin::BaseController
+
     class RetailStoresController < ResourceController
 
+
       def new
-          @retail_store = RetailStore.new
-          @retail_store.address = Address.build_default
+        @retail_store = Spree::RetailStore.new
+        @retail_store.address = Address.build_default
       end
-
+      #
       def index
-
+        @retails_stores = Spree::RetailStore.all
       end
-
-
+      #
+      # def edit
+      #
+      # end
+      #
+      # def update
+      #
+      # end
+      #
+      # def destroy
+      #
+      # end
+      #
+      #
+      #
+      #
       def create
 
         byebug
-        @retail_store = RetailStore.new(permitted_params)
+        @retail_store = Spree::RetailStore.new(permitted_params)
+
+
+        if(@retail_store.save)
+          redirect_to admin_retail_stores_path
+        else
+          render 'new'
+        end
 
       end
-
-
-      private
-
+      #
+      #
+      # private
+      #
       def permitted_params
-        params.require(:retail_store).permit(:name, :timings, :address_attributes)
+        params.require(:retail_store).permit(:name, :timings, address_attributes: [:firstname, :lastname, :address1, :address2, :city, :country_id, :zipcode, :state, :phone, :alternative_phone, :longitude, :latitude])
+        # {"firstname"=>"Sanad", "lastname"=>"Liaquatt", "address1"=>"4163 Irvington Ave", "address2"=>"Apt 109", "city"=>"fremont",
+        # "country_id"=>"232", "zipcode"=>"94538", "state"=>"California", "phone"=>"6305579771", "alternative_phone"=>"6305579771", "longitude"=>"124", "latitude"=>"3234"}
       end
-
 
     end
   end
--- app/models/retail_store.rb
+++
@@ -1,7 +0,0 @@
-
-class Spree::RetailStore < Spree::Base
-
-  belongs_to :address, class_name: 'Spree::Address'
-  accepts_nested_attributes_for :address, :allow_destroy => true
-
-end

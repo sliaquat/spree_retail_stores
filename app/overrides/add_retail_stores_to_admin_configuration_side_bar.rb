0d3b293696fbdb5edf3085bf566891f0c93fd97d
Sanad Liaquat <sanadhussain@gmail.com>
2015-03-10 17:17:26 +0500

    Bug fix

--- app/overrides/add_retail_stores_to_admin_configuration_side_bar.rb
+++ app/overrides/add_retail_stores_to_admin_configuration_side_bar.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(virtual_path: "spree/admin/shared/_configuration_menu",
+Deface::Override.new(virtual_path: "spree/admin/shared/sub_menu/_configuration",
                      name: "retail_stores_added_to_configuration_menu",
-                     insert_bottom: ".sidebar",
+                     insert_bottom: "[data-hook='admin_configurations_sidebar_menu']",
                      partial: "spree/admin/shared/retail_store_settings"
 )
